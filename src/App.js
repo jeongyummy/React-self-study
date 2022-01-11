9d9a1b37c14d71bcd2fcb78213b05f1ead2513f4
Yeoony <83230013+jeongyummy@users.noreply.github.com>
2022-01-11 10:58:04 +0900

    useRef로 컴포넌트 안의 변수 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import "./App.css";
-import InputSameple from "./components/InputSample";
+import React, { useRef } from "react";
+import UserList from "./UserList";
 
 function App() {
-  return (
-    <div className="App">
-      <InputSameple />
-    </div>
-  );
+  const users = [
+    {
+      id: 1,
+      username: "jeongyeon",
+      email: "jeong@gamil.com",
+    },
+    {
+      id: 2,
+      username: "ohohoohoh",
+      email: "oh@gamil.com",
+    },
+    {
+      id: 3,
+      username: "pizza",
+      email: "hungry@gamil.com",
+    },
+  ];
+
+  const nextId = useRef(4); // 파라미터 넣어주면 current 의 기본 값이 된다.
+  const onCreate = () => {
+    nextId.current += 1;
+  };
+
+  return <UserList users={users} />;
 }
 
 export default App;
