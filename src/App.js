8f4cd53354e8ce9f66d7785b98cbbe18f1e1b4cf
Yeoony <83230013+jeongyummy@users.noreply.github.com>
2022-01-11 16:01:56 +0900

    useRef로 다음 DOM 위치 찾아 배열 끝에 항목 추가하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import "./App.css";
-import React, { useRef } from "react";
-import UserList from "./UserList";
+import React, { useRef, useState } from "react";
+import UserList from "./components/UserList";
+import CreateUser from "./components/CreateUser";
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: "jeongyeon",
@@ -19,14 +31,36 @@ function App() {
       username: "pizza",
       email: "hungry@gamil.com",
     },
-  ];
+  ]);
 
   const nextId = useRef(4); // 파라미터 넣어주면 current 의 기본 값이 된다.
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email, // 배열에 추가할 목록들
+    };
+    setUsers([...users, user]);
+    // setUsers(users.concat(user)); + concat 써서도 할 수 있음(기존의 배열을 수정하지 않고, 새로운 원소가 추가된 새로운 배열 생성)
+
+    setInputs({
+      username: "",
+      email: "",
+    });
     nextId.current += 1;
   };
 
-  return <UserList users={users} />;
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
+  );
 }
 
 export default App;
